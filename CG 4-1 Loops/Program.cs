f348636e70d2d9eb5e130b443fca234dbda4a805
Sandra <snelson3671@yahoo.com>
2018-08-08 19:53:10 -0500

    Prints multiples and takes the sum

--- CG 4-1 Loops/Program.cs
+++ CG 4-1 Loops/Program.cs
@@ -6,7 +6,28 @@ namespace CG_4_1_Loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number, and I will give you the first 18 multiples and add them together.");
+
+            string userValue = Console.ReadLine();
+            int x = Convert.ToInt32(userValue);
+
+            {
+                for (int i = 2; i < 20; i++)
+                {
+                    Console.WriteLine(i * x);
+                }
+            }
+            int sum = 0;
+            for (int i = 1; i < 21; i++)
+            {
+            if (i < 21)
+                {
+                sum = sum + i * x;
+                }
+            }
+
+            Console.WriteLine($"The sum is {sum}");
+            Console.ReadLine();
         }
     }
 }
